61fc189984eb8d6baacf0e1b66743633cb5964b3
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-07-07 23:32:01 +0300

    Add Navigation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React, {useEffect, lazy, Suspense} from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/privateRoute';
 import PublicRoute from './components/publicRoute';
+import Navigation from './components/Navigation/Navigation';
 
-// const HomePage = lazy(() => import('./pages/homePage' /* webpackChunkName: "Home-Page" */));
-const RegisterPage = lazy(() => import('./pages/registrationPage' /* webpackChunkName: "Registration-Page" */));
+const HomePage = lazy(() =>
+  import('./pages/HomePage' /* webpackChunkName: "Home-Page" */),
+);
+const DiagramPage = lazy(() =>
+  import('./pages/DiagramPage' /* webpackChunkName: "Diagram-Page" */),
+);
+const RegisterPage = lazy(() =>
+  import(
+    './pages/registrationPage' /* webpackChunkName: "Registration-Page" */
+  ),
+);
 // const LoginPage = lazy(() => import('./pages/loginPage' /* webpackChunkName: "Login-Page" */));
 
 const FinanceApp = () => {
-    return (
-        <Suspense fallback={<p>Загружаем...</p>} >
-            <section>
-                <Switch>
-                    {/* <Route exact path='/' render={(props) => <HomePage {...props} />} /> */}
-                    <PublicRoute path='/register' restricted component={RegisterPage} redirectTo='/contacts' />
-                    {/* <PublicRoute path='/login' restricted component={LoginPage} redirectTo='/contacts' /> */}
-                </Switch>
-            </section>
-        </Suspense>
-    );
+  return (
+    <>
+      <Navigation />
+      <Suspense fallback={<p>Загружаем...</p>}>
+        <section>
+          <Switch>
+            <Route exact path={'/home'} component={HomePage}></Route>
+            <Route exact path={'/diagram'} component={DiagramPage}></Route>
+            <PublicRoute
+              path="/register"
+              restricted
+              component={RegisterPage}
+              redirectTo="/contacts"
+            />
+            {/* <PublicRoute path='/login' restricted component={LoginPage} redirectTo='/contacts' /> */}
+            <Route component={HomePage}></Route>
+          </Switch>
+        </section>
+      </Suspense>
+    </>
+  );
 };
 
 export default FinanceApp;
---
+++ src/pages/HomePage.js
@@ -0,0 +1,4 @@
+const HomePage = () => {
+  return <h2>Главная страница</h2>;
+};
+export default HomePage;
