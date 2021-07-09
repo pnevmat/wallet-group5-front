92a154ffb453ce474313fdaf5f560f3c8ec5a6ee
alinaYamkova <alyamkova@yahoo.com>
2021-07-09 14:29:37 +0200

    add spinner, header, homepage etc.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import React, {useEffect, lazy, Suspense} from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { useDispatch } from 'react-redux';
-import {Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './components/privateRoute';
 import PublicRoute from './components/publicRoute';
+import Navigation from './components/Navigation/Navigation';
 
-// const HomePage = lazy(() => import('./pages/homePage' /* webpackChunkName: "Home-Page" */));
-const RegisterPage = lazy(() => import('./pages/registrationPage' /* webpackChunkName: "Registration-Page" */));
-const LoginPage = lazy(() => import('./pages/loginPage' /* webpackChunkName: "Login-Page" */));
+import authOperations from '../src/redux/operations/loginOperation';
+import { useDispatch } from 'react-redux';
+import AppBar from '../src/components/AppBar/AppBar';
+import routes from '../src/routes';
+import Spinner from './Spinner';
+
+const HomePage = lazy(() =>
+  import('./pages/MainPage' /* webpackChunkName: "Home-Page" */),
+);
+const RegisterPage = lazy(() =>
+  import(
+    './pages/registrationPage' /* webpackChunkName: "Registration-Page" */
+  ),
+);
+const LoginPage = lazy(() =>
+  import('./pages/loginPage' /* webpackChunkName: "Login-Page" */),
+);
+const DashboardPage = lazy(() =>
+  import('./pages/dashboardPage' /* webpackChunkName: "Dashboard-Page" */),
+);
+const StatisticsPage = lazy(() =>
+  import('./pages/statisticsPage' /* webpackChunkName: "Statistics-Page" */),
+);
 
 const FinanceApp = () => {
-    return (
-        <Suspense fallback={<p>Загружаем...</p>} >
-            <section>
-                <Switch>
-                    {/* <Route exact path='/' render={(props) => <HomePage {...props} />} /> */}
-                    <PublicRoute path='/register' restricted component={RegisterPage} redirectTo='/contacts' />
-                    <PublicRoute path='/login' restricted component={LoginPage} redirectTo='/contacts' />
-                </Switch>
-            </section>
-        </Suspense>
-    );
+  return (
+    <Suspense fallback={<Spinner />}>
+      <AppBar />
+      <section>
+        <Switch>
+          <Route exact path={routes.home}>
+            <HomePage />
+          </Route>
+
+          <PublicRoute
+            path="/register"
+            restricted
+            component={RegisterPage}
+            redirectTo="/contacts"
+          />
+          <PublicRoute
+            path="/login"
+            restricted
+            component={LoginPage}
+            redirectTo="/contacts"
+          />
+          <PrivateRoute
+            path="/dashboard"
+            restricted
+            component={DashboardPage}
+            redirectTo="/login"
+          />
+          <PrivateRoute
+            path="/statistics"
+            restricted
+            component={StatisticsPage}
+            redirectTo="/login"
+          />
+        </Switch>
+      </section>
+    </Suspense>
+  );
 };
 
 export default FinanceApp;
--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ import {Provider} from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import {PersistGate} from 'redux-persist/integration/react';
 import store from './redux/store';
-
 import FinanceApp from './App';
-
 import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.render(
