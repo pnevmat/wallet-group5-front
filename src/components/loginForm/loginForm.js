30e6e59292b177b3ac92a821629fad30700d05f7
Volodymyr Molodynia <molodunja91@gmail.com>
2021-07-10 12:34:15 +0300

    add stiles desktop and tablet

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 import { NavLink } from "react-router-dom";
+import s from  './loginForm.module.css'
+
+import wallet from './images/wallet.png'
 
 const LoginForm = (props) => {
     const [email, setEmail] = useState('');
@@ -26,23 +29,28 @@ const LoginForm = (props) => {
     };
 
     return (
-        <div>
-            <form onSubmit={(e) => {
+        <div className={s.container}>
+            
+            <form className={s.form} onSubmit={(e) => {
                 e.preventDefault();
                 handleSubmit()
             }}>
+                <div className={s.logo}>
+                <img className={s.logo__img} src={wallet} alt='logo' width='40px' height='40px'/>
+                <h1 className={s.logo__name}>Wallet</h1>
+            </div>
                 <label htmlFor="" name="register" >
-                    <input type="text" placeholder="E-mail" name="email" onChange={e => {
+                    <input className={s.input__email} type="text" placeholder="E-mail" name="email" onChange={e => {
                         handleChange(e);
                     }}/>
                 </label>
                 <label>
-                    <input  type="text" placeholder="Пароль" name="password" onChange={e => {
+                    <input className={s.input__password}  type="text" placeholder="Пароль" name="password" onChange={e => {
                         handleChange(e);
                     }}/>
                 </label>
-                <button type="submit">Вход</button>
-                <NavLink to='/register'>Регистрация</NavLink>
+                <button className={s.button__submit} type="submit">Вход</button>
+                <NavLink className={s.button__registration }  to='/register'>Регистрация</NavLink>
             </form>
         </div>
     );
