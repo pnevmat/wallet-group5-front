65972120783b5828e31d7e1f6b689c9da0e14d34
Volodymyr Molodynia <molodunja91@gmail.com>
2021-07-11 12:28:51 +0300

    add stiles mobile

--- src/components/loginForm/loginForm.js
+++ src/components/loginForm/loginForm.js
@@ -1,59 +1,87 @@
-import React, {useState} from 'react';
-import { NavLink } from "react-router-dom";
-import s from  './loginForm.module.css'
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import s from './loginForm.module.css';
 
-import wallet from './images/wallet.png'
+import wallet from './images/wallet.png';
 
-const LoginForm = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+const LoginForm = props => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleChange = e => {
-        const {name, value} = e.target;
-        switch(name) {
-            case 'email':
-                setEmail(value);
-                break;
-            case 'password':
-                setPassword(value);
-                break;
-            default:
-                console.log('error');
-        };
-    };
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+        setPassword(value);
+        break;
+      default:
+        console.log('error');
+    }
+  };
 
-    const handleSubmit = () => {
-        const {onLoginSubmit} = props;
+  const handleSubmit = () => {
+    const { onLoginSubmit } = props;
 
-        onLoginSubmit({email, password});
-    };
+    onLoginSubmit({ email, password });
+  };
 
-    return (
-        <div className={s.container}>
-            
-            <form className={s.form} onSubmit={(e) => {
-                e.preventDefault();
-                handleSubmit()
-            }}>
-                <div className={s.logo}>
-                <img className={s.logo__img} src={wallet} alt='logo' width='40px' height='40px'/>
-                <h1 className={s.logo__name}>Wallet</h1>
-            </div>
-                <label htmlFor="" name="register" >
-                    <input className={s.input__email} type="text" placeholder="E-mail" name="email" onChange={e => {
-                        handleChange(e);
-                    }}/>
-                </label>
-                <label>
-                    <input className={s.input__password}  type="text" placeholder="Пароль" name="password" onChange={e => {
-                        handleChange(e);
-                    }}/>
-                </label>
-                <button className={s.button__submit} type="submit">Вход</button>
-                <NavLink className={s.button__registration }  to='/register'>Регистрация</NavLink>
-            </form>
+  return (
+    <div className={s.container}>
+      <form
+        className={s.form}
+        onSubmit={e => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <div className={s.logo}>
+          <img
+            className={s.logo__img}
+            src={wallet}
+            alt="logo"
+            width="40px"
+            height="40px"
+          />
+          <h1 className={s.logo__name}>Wallet</h1>
         </div>
-    );
+        <label htmlFor="" name="register">
+          <input
+            className={s.input__email}
+            type="text"
+            placeholder="E-mail"
+            name="email"
+            onChange={e => {
+              handleChange(e);
+            }}
+          />
+        </label>
+        <label>
+          <input
+            className={s.input__password}
+            type="text"
+            placeholder="Пароль"
+            name="password"
+            onChange={e => {
+              handleChange(e);
+            }}
+          />
+        </label>
+        <button className={s.button__submit}  activeClassName={s.active} type="submit">
+          Вход
+        </button>
+        <NavLink 
+        className={s.button__registration} 
+        to="/register"
+        activeClassName={s.active}
+        >
+          Регистрация
+        </NavLink>
+      </form>
+    </div>
+  );
 };
 
 export default LoginForm;
--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,10 +1,10 @@
-import {React, useState, useEffect} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import frame from '../components/loginForm/images/frame.png'
-import s from '../components/loginForm/loginForm.module.css'
+import { React, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import frame from '../components/loginForm/images/frame.png';
+import s from '../components/loginForm/loginForm.module.css';
 
-import eclipseTablet from '../components/loginForm/images/frameTablet.png'
-import frameTablet from '../components/loginForm/images/eclipseTablet.png'
+import eclipseTablet from '../components/loginForm/images/frameTablet.png';
+import frameTablet from '../components/loginForm/images/eclipseTablet.png';
 
 import loginOperation from '../redux/operations/loginOperation';
 import selectors from '../redux/selectors/registrationSelectors/registrationSelectors';
@@ -12,34 +12,42 @@ import selectors from '../redux/selectors/registrationSelectors/registrationSele
 import LoginForm from '../components/loginForm/loginForm';
 
 const LoginPage = () => {
-    const dispatch = useDispatch();
-    const onLoginSubmit = userData => dispatch(loginOperation(userData));
+  const dispatch = useDispatch();
+  const onLoginSubmit = userData => dispatch(loginOperation(userData));
 
-    const error = useSelector(selectors.registrationSelector);
+  const error = useSelector(selectors.registrationSelector);
 
-    if (error && typeof error === 'string') {
-        alert(`${error}`);
-    }
+  if (error && typeof error === 'string') {
+    alert(`${error}`);
+  }
 
-    return (
-        <>
-        <div className={s.containerloginPages}>
-            
+  return (
+    <>
+      <div className={s.containerloginPages}>
         <div className={s.loginPages}>
-            <div className={s.logiPagedox}>
-            <img className={s.logiPagedoxImg1} src={eclipseTablet} alt="gfgfg"/>
-            <img className={s.logiPagedoxImg2} src={frameTablet} alt="gklg"/>
-            </div>
-           
-        <div className={s.loginPageVrap}></div>
-                <img className={s.loginPagesImg} src={frame} alt='кошелек' width='436' height='420'/>
-                <h1 className={s.loginPageName}>Finance</h1>
-            </div>
-            <LoginForm onLoginSubmit={onLoginSubmit} />
+          <div className={s.logiPagedox}>
+            <img
+              className={s.logiPagedoxImg1}
+              src={eclipseTablet}
+              alt="gfgfg"
+            />
+            <img className={s.logiPagedoxImg2} src={frameTablet} alt="gklg" />
+          </div>
+
+          <div className={s.loginPageVrap}></div>
+          <img
+            className={s.loginPagesImg}
+            src={frame}
+            alt="кошелек"
+            width="436"
+            height="420"
+          />
+          <h1 className={s.loginPageName}>Finance App</h1>
         </div>
-            
-        </>
-    );
+        <LoginForm onLoginSubmit={onLoginSubmit} />
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;
