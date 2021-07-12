53e3fe8d3ec80dcbd0b53c462c304ad7d82becef
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-07-12 18:23:42 +0300

    Add Balance

---
+++ src/components/balance/balance.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './balance.module.css';
+
+const Balance = props => {
+  return (
+    <div className={s.conteiner}>
+      <p className={s.title}>ВАШ БАЛАНС</p>
+      <p className={s.balance}>₴ {props.balance}</p>
+    </div>
+  );
+};
+export default Balance;
--- src/redux/selectors/authorisationSelectors/authorisationSelectors.js
+++ src/redux/selectors/authorisationSelectors/authorisationSelectors.js
@@ -1,15 +1,28 @@
-import {createSelector} from '@reduxjs/toolkit';
+import { createSelector } from '@reduxjs/toolkit';
 
 const authorisation = state => state.session.isAuth;
 const getUserName = state => state.userData.registrationData.name;
 const getUserEmail = state => state.auth.user.email;
+const getUserBalance = state => state.userData.registrationData.balance;
 
 const authorisationSelector = createSelector([authorisation], authorised => {
-    return authorised
+  return authorised;
 });
 
 const userNameSelector = createSelector([getUserName], userName => {
-    return userName
+  return userName;
 });
 
-export default {authorisation, getUserName, getUserEmail, authorisationSelector, userNameSelector};
+const userBalanceSelector = createSelector([getUserBalance], balace => {
+  return balace;
+});
+
+export default {
+  authorisation,
+  getUserName,
+  getUserEmail,
+  getUserBalance,
+  authorisationSelector,
+  userNameSelector,
+  userBalanceSelector,
+};
