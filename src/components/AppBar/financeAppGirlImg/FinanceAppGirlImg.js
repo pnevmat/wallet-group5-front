c7e519c2190fd4a95393b6fa6c81760c01cdf9a8
Volodymyr Molodynia <molodunja91@gmail.com>
2021-07-15 21:46:30 +0300

    refactor pullrequest

--- src/components/AppBar/financeAppGirlImg/FinanceAppGirlImg.js
+++ src/components/AppBar/financeAppGirlImg/FinanceAppGirlImg.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import s from './financeAppGirlImg.module.css'
+import eclipseTablet from './images/frameTablet.png';
+import frameTablet from './images/eclipseTablet.png';
+import frame from './images/frame.png';
 
 const FinanceAppGirlImg = () => {
     return (
         <>
+        <div className={s.containerloginPages}>
+            <div className={s.loginPages}>
+                <div className={s.logiPagedox}>
+                    <img
+                    className={s.logiPagedoxImg1}
+                    src={eclipseTablet}
+                    alt="gfgfg"
+                    />
+                    <img className={s.logiPagedoxImg2} src={frameTablet} alt="gklg" />
+                </div>
+                <div className={s.loginPageVrap}></div>
+                <img
+                    className={s.loginPagesImg}
+                    src={frame}
+                    alt="кошелек"
+                    width="436"
+                    height="420"
+                />
+                <h1 className={s.loginPageName}>Finance App</h1>
+            </div>
+        </div>
         </>
     );
 };
