a834a5928faf00473437ed8cc4dae7ca557c3632
pnevmat <pnevmat001@gmail.com>
2024-01-19 17:06:59 +0200

    changes base url for production

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://goit-hw-wallet-group5-back.onrender.com';
+// 'http://localhost:5000'
 // 'https://goit-hw-wallet-group5-back.onrender.com';
 
 export default BASE_URL;
